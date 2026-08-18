8288fbef94720cfb47fffd951d5f692d9907f56a
Andriy Shyrokoryadov <a.shyrokoriadov@gmail.com>
2026-08-18 19:21:55 +0200

    Added additional logging in MQTT client.

--- SmartHome.Arduino.DataCollector/MqttService.cpp
+++ SmartHome.Arduino.DataCollector/MqttService.cpp
@@ -7,6 +7,18 @@ bool MqttService::begin(const char* brokerHost, uint16_t port)
     client.setClient(wifiClient);
     client.setServer(brokerHost, port);
 
+    // quick TCP check to broker before attempting MQTT connect
+    {
+        WiFiClient testClient;
+        Serial.println("Testing TCP connection to MQTT...");
+        if (testClient.connect(brokerHost, port)) {
+            Serial.println("TCP connection SUCCESS");
+            testClient.stop();
+        } else {
+            Serial.println("TCP connection FAILED");
+        }
+    }
+
     if (!client.connect("SmartHomeCollector")) {
         Serial.print("MQTT connection failed, state = ");
         Serial.println(client.state());
@@ -37,6 +49,18 @@ void MqttService::reconnectIfNeeded()
 {
     if (!client.connected()) {
         Serial.println("MQTT reconnect attempt.");
+        // quick TCP check to broker before attempting MQTT reconnect
+        {
+            WiFiClient testClient;
+            Serial.println("Testing TCP connection to MQTT...");
+            if (testClient.connect(MQTT_BROKER, MQTT_PORT)) {
+                Serial.println("TCP connection SUCCESS");
+                testClient.stop();
+            } else {
+                Serial.println("TCP connection FAILED");
+            }
+        }
+
         if (!client.connect("SmartHomeCollector")) {
             Serial.print("MQTT reconnect failed, state = ");
             Serial.println(client.state());
