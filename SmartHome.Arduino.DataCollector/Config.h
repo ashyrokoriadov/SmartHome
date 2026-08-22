93c175d23fdc3b97d8c682d85f6e9ae9420d7cc5
Andriy Shyrokoryadov <a.shyrokoriadov@gmail.com>
2026-08-22 19:19:44 +0200

    Changed data reading interval.

--- SmartHome.Arduino.DataCollector/Config.h
+++ SmartHome.Arduino.DataCollector/Config.h
@@ -17,8 +17,8 @@ constexpr uint8_t LAMPS_CONTROL_PIN = 7;
 constexpr uint8_t VICTRON_RX_PIN = 0;
 constexpr uint8_t VICTRON_TX_PIN = 1;
 
-constexpr unsigned long SENSOR_INTERVAL_MS = 10UL * 1000UL;
-constexpr unsigned long VICRON_INTERVAL_MS = 10UL * 1000UL;
+constexpr unsigned long SENSOR_INTERVAL_MS = 30UL * 1000UL;
+constexpr unsigned long VICRON_INTERVAL_MS = 30UL * 1000UL;
 constexpr unsigned long WIFI_RETRY_MS = 5UL * 1000UL;
 constexpr unsigned long MQTT_RETRY_MS = 10UL * 1000UL;
 
